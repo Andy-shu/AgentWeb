2c4e99fb906520eaf6f200a19d3c7b61ade41555
Andy-shu <Andy-shu@users.noreply.github.com>
2017-10-31 17:30:27 +0800

    Revert "舒飞"
    This reverts commit d089ed5d975cfd6903c08921cccc919c58fe893d.

--- sample/src/main/java/com/just/library/agentweb/BaseWebActivity.java
+++ sample/src/main/java/com/just/library/agentweb/BaseWebActivity.java
@@ -26,7 +26,7 @@ import com.just.library.LogUtils;
 /**
  * Created by cenxiaozhong on 2017/5/26.
  *
- * source CODE https://github.com/Justson/AgentWeb
+ * source CODE  https://github.com/Justson/AgentWeb
  *
  * <p>
  */
@@ -35,150 +35,158 @@ public class BaseWebActivity extends AppCompatActivity {
 
 
 
-  protected AgentWeb mAgentWeb;
-  private LinearLayout mLinearLayout;
-  private Toolbar mToolbar;
-  private TextView mTitleTextView;
-  private AlertDialog mAlertDialog;
+    protected AgentWeb mAgentWeb;
+    private LinearLayout mLinearLayout;
+    private Toolbar mToolbar;
+    private TextView mTitleTextView;
+    private AlertDialog mAlertDialog;
 
-
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    setContentView(R.layout.activity_web);
-
-
-    mLinearLayout = (LinearLayout) this.findViewById(R.id.container);
-    mToolbar = (Toolbar) this.findViewById(R.id.toolbar);
-    mToolbar.setTitleTextColor(Color.WHITE);
-    mToolbar.setTitle("");
-    mTitleTextView = (TextView) this.findViewById(R.id.toolbar_title);
-    this.setSupportActionBar(mToolbar);
-    if (getSupportActionBar() != null)
-      // Enable the Up button
-      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-    mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-
-        showDialog();
-      }
-    });
-
-
-    long p = System.currentTimeMillis();
-
-    mAgentWeb = AgentWeb.with(this)//
-        .setAgentWebParent(mLinearLayout, new LinearLayout.LayoutParams(-1, -1))//
-        .useDefaultIndicator()//
-        .defaultProgressBarColor().setReceivedTitleCallback(mCallback)
-        .setWebChromeClient(mWebChromeClient).setWebViewClient(mWebViewClient)
-        .setSecutityType(AgentWeb.SecurityType.strict).setWebLayout(new WebLayout(this))
-        .createAgentWeb()//
-        .ready().go(getUrl());
-
-    // mAgentWeb.getLoader().loadUrl(getUrl());
-
-    long n = System.currentTimeMillis();
-    Log.i("Info", "init used time:" + (n - p));
-
-
-
-  }
-
-  private WebViewClient mWebViewClient = new WebViewClient() {
-    @Override
-    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-      return super.shouldOverrideUrlLoading(view, request);
-    }
 
     @Override
-    public void onPageStarted(WebView view, String url, Bitmap favicon) {
-      // do you work
-      Log.i("Info", "BaseWebActivity onPageStarted");
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_web);
+
+
+        mLinearLayout = (LinearLayout) this.findViewById(R.id.container);
+        mToolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        mToolbar.setTitle("");
+        mTitleTextView = (TextView) this.findViewById(R.id.toolbar_title);
+        this.setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null)
+            // Enable the Up button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showDialog();
+            }
+        });
+
+
+        long p = System.currentTimeMillis();
+
+        mAgentWeb = AgentWeb.with(this)//
+                .setAgentWebParent(mLinearLayout,new LinearLayout.LayoutParams(-1,-1) )//
+                .useDefaultIndicator()//
+                .defaultProgressBarColor()
+                .setReceivedTitleCallback(mCallback)
+                .setWebChromeClient(mWebChromeClient)
+                .setWebViewClient(mWebViewClient)
+                .setSecutityType(AgentWeb.SecurityType.strict)
+                .setWebLayout(new WebLayout(this))
+                .createAgentWeb()//
+                .ready()
+                .go(getUrl());
+
+        //mAgentWeb.getLoader().loadUrl(getUrl());
+
+        long n = System.currentTimeMillis();
+        Log.i("Info", "init used time:" + (n - p));
+
+
+
+
+
     }
-  };
-  private WebChromeClient mWebChromeClient = new WebChromeClient() {
-    @Override
-    public void onProgressChanged(WebView view, int newProgress) {
-      // do you work
-      // Log.i("Info","progress:"+newProgress);
+
+    private WebViewClient mWebViewClient=new WebViewClient(){
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+           //do you  work
+            Log.i("Info","BaseWebActivity onPageStarted");
+        }
+    };
+    private WebChromeClient mWebChromeClient=new WebChromeClient(){
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            //do you work
+//            Log.i("Info","progress:"+newProgress);
+        }
+    };
+    public String getUrl(){
+
+        return "https://m.jd.com/";
     }
-  };
 
-  public String getUrl() {
-
-    return "https://m.jd.com/";
-  }
-
-  private ChromeClientCallbackManager.ReceivedTitleCallback mCallback =
-      new ChromeClientCallbackManager.ReceivedTitleCallback() {
+    private ChromeClientCallbackManager.ReceivedTitleCallback mCallback = new ChromeClientCallbackManager.ReceivedTitleCallback() {
         @Override
         public void onReceivedTitle(WebView view, String title) {
-          if (mTitleTextView != null) mTitleTextView.setText(title);
+            if (mTitleTextView != null)
+                mTitleTextView.setText(title);
         }
-      };
+    };
 
 
-  private void showDialog() {
+    private void showDialog() {
 
-    if (mAlertDialog == null) mAlertDialog = new AlertDialog.Builder(this).setMessage("您确定要关闭该页面吗?")
-        .setNegativeButton("再逛逛", new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialog, int which) {
-            if (mAlertDialog != null) mAlertDialog.dismiss();
-          }
-        })//
-        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialog, int which) {
+        if (mAlertDialog == null)
+            mAlertDialog = new AlertDialog.Builder(this)
+                    .setMessage("您确定要关闭该页面吗?")
+                    .setNegativeButton("再逛逛", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (mAlertDialog != null)
+                                mAlertDialog.dismiss();
+                        }
+                    })//
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
 
-            if (mAlertDialog != null) mAlertDialog.dismiss();
+                            if (mAlertDialog != null)
+                                mAlertDialog.dismiss();
 
-            BaseWebActivity.this.finish();
-          }
-        }).create();
-    mAlertDialog.show();
+                            BaseWebActivity.this.finish();
+                        }
+                    }).create();
+        mAlertDialog.show();
 
-  }
-
-  @Override
-  public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-    if (mAgentWeb.handleKeyEvent(keyCode, event)) {
-      return true;
     }
-    return super.onKeyDown(keyCode, event);
-  }
 
-  @Override
-  protected void onPause() {
-    mAgentWeb.getWebLifeCycle().onPause();
-    super.onPause();
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-  }
+        if (mAgentWeb.handleKeyEvent(keyCode, event)) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
-  @Override
-  protected void onResume() {
-    mAgentWeb.getWebLifeCycle().onResume();
-    super.onResume();
-  }
+    @Override
+    protected void onPause() {
+        mAgentWeb.getWebLifeCycle().onPause();
+        super.onPause();
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    }
 
-    Log.i("Info", "result:" + requestCode + " result:" + resultCode);
-    mAgentWeb.uploadFileResult(requestCode, resultCode, data);
-    super.onActivityResult(requestCode, resultCode, data);
-  }
+    @Override
+    protected void onResume() {
+        mAgentWeb.getWebLifeCycle().onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.i("Info", "result:" + requestCode + " result:" + resultCode);
+        mAgentWeb.uploadFileResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
 
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    // mAgentWeb.destroy();
-    mAgentWeb.getWebLifeCycle().onDestroy();
-  }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //mAgentWeb.destroy();
+        mAgentWeb.getWebLifeCycle().onDestroy();
+    }
 }
